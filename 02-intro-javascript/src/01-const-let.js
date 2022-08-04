944e0a7ef16a6a282ec837aa260abc51bb3f8244
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-08-04 09:38:23 -0400

    Creacion 02-intro Constantes y variables

---
+++ 02-intro-javascript/src/01-const-let.js
@@ -0,0 +1,17 @@
+console.log('Hola mundo');
+
+//Variables y constantes
+
+const nombre = 'Rodrigo';
+let apellido = 'Otárola';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+
+if(true){
+    let valorDado =6;
+    console.log(valorDado);
+}
+console.log(valorDado);
