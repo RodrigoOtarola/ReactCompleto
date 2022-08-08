15c681795a95eb395ed9ec2f15303cb375a14b0f
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-08-08 17:23:20 -0400

    fetch api

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -210,3 +210,26 @@
 // getHeroeByIdAsync(10)
 //     .then(console.log)
 //     .catch(console.warn);
+
+// //Sacada desde giphy
+// const apiKey= 'Y96RP1YAwBhEGfnqruzkBjc21wnX4G65';
+//
+// //peticion fetch api
+// const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+//
+// //respuesta encadenada
+// peticion.then(resp=>resp.json()
+// )
+//     //Recuperar url de la imagen
+//     .then(({data})=>{
+//         const {url} = data.images.original;
+//         console.log(url);
+//
+//         //guarda url de imagen en memoria
+//         const img = document.createElement('img');
+//         img.src = url;
+//
+//         //Mostrar imagen en el html
+//         document.body.append(img);
+//     })
+//     .catch(console.warn);
