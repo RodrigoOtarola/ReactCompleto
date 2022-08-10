8dbe180f2e404a9b615cc2f0fa361350f3a1dda5
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-08-09 22:11:16 -0400

    hola mundo

--- 03-counter-app-vite/src/index.js
+++ 03-counter-app-vite/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Para renderizar componentes de react
+import {createRoot} from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Componente inicial.
+const saludo =<h1>Hola mundo</h1>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Renderizar componente
+const divRoot = createRoot(document.getElementById('root'));
+
+divRoot.render(saludo);
