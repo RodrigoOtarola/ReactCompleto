4a02ef8ebc4bba42644e87a56d7a2bdaee8b2670
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-08-12 14:45:39 -0400

    impresion de variable

--- 03-counter-app-vite/src/firstApp.jsx
+++ 03-counter-app-vite/src/firstApp.jsx
@@ -1,11 +1,28 @@
 // Para tener dos elementos en la raiz del functional component
 //import {Fragment} from "react";
 
+// export const firstApp = () => {
+//     return (
+//         <>
+//             <h2>Nombre: Rodrigo</h2>
+//             <p>Soy un subtitulo</p>
+//         </>
+//     );
+// }
+
+const newMessage ={
+    message: 'Hola mundo',
+    title: 'Rodrigo'
+}
+
 export const firstApp = () => {
+
+
     return (
         <>
-            <h2>Nombre: Rodrigo</h2>
-            <p>Soy un subtitulo</p>
+            {/*<h2>{JSON.stringify(newMessage)}</h2>*/}
+            <h2>{newMessage.message}</h2>
+            <p>mi nombre es: {newMessage.title}</p>
         </>
     );
 }
