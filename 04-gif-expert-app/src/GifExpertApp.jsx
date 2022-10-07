d5a2323fd40f55d4a750e18c4af79980eec34c50
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-10-07 17:54:27 -0300

    insertar categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -5,12 +5,21 @@ export const GifExpertApp = () => {
   //Inicializar el useState no se recomienda dejarlo vacio
   const [categories, setCategories] = useState(["One Punch","Prueba"]);
 
+  //Agregar categories
+  const onAddCategory =()=>{
+    //Mutar el estado, ya que no se puede ocupar push
+    //
+    setCategories( [...categories,'prueba'] );
+  };
+
   return (
     <>
       {/*titulo */}
       <h1>GifExpertApp</h1>
 
       {/*lista*/}
+      <button onClick={onAddCategory}>Agregar</button>
+
       <ol>
         {/*map sirve para barrer los elementos del arreglo y colocar algo nuevo*/}
         {categories.map(category =>{
