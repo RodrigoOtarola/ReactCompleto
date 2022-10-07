d949972620278e0eea8fe64ce7617fad94154f7a
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-10-07 17:29:24 -0300

    lista de categorias

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,11 +1,23 @@
-
-import React from 'react'
+import { useState } from "react";
+import React from "react";
 
 export const GifExpertApp = () => {
+  //Inicializar el useState no se recomienda dejarlo vacio
+  const [categories, setCategories] = useState(["One Punch","Prueba"]);
+
   return (
     <>
-    {/*titulo */}
+      {/*titulo */}
       <h1>GifExpertApp</h1>
+
+      {/*lista*/}
+      <ol>
+        {/*map sirve para barrer los elementos del arreglo y colocar algo nuevo*/}
+        {categories.map(category =>{
+            return(<li key={category}>{category}</li>)
+        })}
+
+      </ol>
     </>
-  )
-}
+  );
+};
