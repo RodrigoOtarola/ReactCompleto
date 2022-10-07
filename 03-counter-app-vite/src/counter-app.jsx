486d93a932f89656baeeac6529664e4fab9a5b3e
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-10-07 10:28:27 -0300

    button con onClick

--- 03-counter-app-vite/src/counter-app.jsx
+++ 03-counter-app-vite/src/counter-app.jsx
@@ -1,14 +1,28 @@
- import PropTypes from 'prop-types';
- 
- export const CounterApp= ({value})=>{
-    return(
-        <> 
-            <h1>CounterApp</h1>
-            <h2>{value}</h2>
-        </>
-    )
-}
+import PropTypes from "prop-types";
+
+export const CounterApp = ({ value }) => {
+  //Se llama desde función onClick del button y para retornar valor.
+  const handleAdd = (event) => {
+    //console.log(event);
+    //console.log(newValue);
+
+    value = 1000;
+
+  };
+
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{value}</h2>
+      <button
+        onClick={handleAdd}
+      >
+        +1
+      </button>
+    </>
+  );
+};
 
 CounterApp.propTypes = {
-    value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
