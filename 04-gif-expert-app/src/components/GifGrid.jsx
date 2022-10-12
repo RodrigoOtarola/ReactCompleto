01add6d9aee66a771f4c36719ef7e45dde8625de
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-10-12 17:52:05 -0300

    CSS de imagenes de la api

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -2,32 +2,33 @@ import React, { Fragment } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+import "../style.css";
 
 export const GifGrid = ({ category }) => {
-
   const [images, setImages] = useState([]);
 
-  const getImages = async() => {
-    const newImages = await getGifs( category );
+  const getImages = async () => {
+    const newImages = await getGifs(category);
     setImages(newImages);
-  }
-
+  };
 
   //useEffect 1er parametro es una function y al final se pasa arreglo vacio
-  useEffect(()=>{
+  useEffect(() => {
     getImages();
-  },[])
-  
+  }, []);
 
   return (
     <Fragment>
-      <h3>{category}</h3>      
-      <ol>
-        {images.map((image)=>
-          <li key={image.id}>{image.title}</li>
-        )}        
-      </ol>
-
+      <h3>{category}</h3>
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem
+            key={image.id}
+            {...image}
+          />
+        ))}
+      </div>
     </Fragment>
   );
 };
